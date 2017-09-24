46354e830189b3bbe0bf075950ce60c40a7c01d6
Thong Do <dhthong@gmail.com>
2017-09-23 17:30:09 -0700

    new files

---
+++ lambda.js
@@ -0,0 +1,9 @@
+var braintree = require("braintree");
+require('dotenv').config()
+
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "useYourMerchantId",
+  publicKey: "useYourPublicKey",
+  privateKey: "useYourPrivateKey"
+});
