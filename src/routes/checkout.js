01d2164057e63140796a809fb93accec2beff6c7
Thong Do <dhthong@gmail.com>
2017-09-26 11:13:33 -0700

    new files

--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -1,9 +1,12 @@
 const { responseError, responseSuccess, getAccountTable, getPlaid } = require('../helpers/utils');
 const braintree = require("braintree");
 module.exports = [{
-  path: '/api/client-token',
-  method: 'get',
+  path: '/api/checkout',
+  method: 'post',
   handler: (req, res) => {
+    var amount = req.body.amount;
+    var nonce = req.body.nonce;
+    var nameOnCard = req.body.nameoncard;
 
     const gateway = braintree.connect({
       environment: braintree.Environment.Sandbox,
@@ -12,12 +15,17 @@ module.exports = [{
       privateKey: process.env.BT_PRIVATE_KEY
     });
 
-    gateway.clientToken.generate({}, function (err, response) {
+    gateway.transaction.sale({
+      amount: amount,
+      paymentMethodNonce: nonce,
+      cardholderName: nameOnCard,
+      options: {
+        submitForSettlement: true
+      }
+    }, function (err, result) {
       if (err) {
         console.error(err);
         return;
-      }else{
-        res.send(response.clientToken);
       }
     });
   }
