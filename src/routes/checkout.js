32a3d5a186b51c5d3b7fe0602cb7616ffc63d50b
Thong Do <dhthong@gmail.com>
2017-09-26 16:48:12 -0700

    new files

--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -6,7 +6,18 @@ module.exports = [{
   handler: (req, res) => {
     var amount = req.body.amount;
     var nonce = req.body.nonce;
+    var email = req.body.email;
     var nameOnCard = req.body.nameoncard;
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var company = req.body.company;
+    var streetAddress = req.body.streetAddress;
+    var extendedAddress = req.body.extendedAddress;
+    var city = req.body.city;
+    var country = req.body.country;
+    var state = req.body.state;
+    var postalCode = req.body.postalCode;
+    var phone = req.body.phone;
 
     const gateway = braintree.connect({
       environment: braintree.Environment.Sandbox,
@@ -18,14 +29,48 @@ module.exports = [{
     gateway.transaction.sale({
       amount: amount,
       paymentMethodNonce: nonce,
-      cardholderName: nameOnCard,
+      creditCard: {
+        cardholderName: nameOnCard
+      },
+      customer: {
+        firstName: firstname,
+        lastName: lastname,
+        company: company,
+        phone: phone,
+        email: email
+      },
+      billing: {
+        firstName: firstname,
+        lastName: lastname,
+        company: company,
+        streetAddress: streetAddress,
+        extendedAddress: extendedAddress,
+        locality: city,
+        region: state,
+        postalCode: postalCode,
+        countryCodeAlpha2: country
+      },
+      shipping: {
+        firstName: firstname,
+        lastName: lastname,
+        company: company,
+        streetAddress: streetAddress,
+        extendedAddress: extendedAddress,
+        locality: city,
+        region: state,
+        postalCode: postalCode,
+        countryCodeAlpha2: country
+      },
       options: {
-        submitForSettlement: true
+        submitForSettlement: true,
+        storeInVaultOnSuccess: true
       }
     }, function (err, result) {
-      if (err) {
-        console.error(err);
-        return;
+      if (result.success || result.transaction) {
+        console.log("data is saved");
+      } else {
+        transactionErrors = result.errors.deepErrors();
+        req.flash('error', {msg: formatErrors(transactionErrors)});
       }
     });
   }
